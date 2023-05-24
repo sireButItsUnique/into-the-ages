e8ab4ee3d518451d565eea7436043523cbeb6f30
sire <robertcxlee@gmail.com>
2023-05-24 12:46:04 -0600

    set up basic main class

---
+++ custom-mod/src/main/java/com/sireButItsUnique/intotheages/IntoTheAges.java
@@ -0,0 +1,37 @@
+package com.sireButItsUnique.intotheages;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import net.minecraft.init.Blocks;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(useMetadata = true, modid = "into-the-ages", name = "Into The Ages", version = "0.1")
+
+public class IntoTheAges {
+    private static Logger logger = LogManager.getLogger("into-the-ages");
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        logger.info("heyy");
+    }
+
+    @EventHandler
+    public void postInit(FMLInitializationEvent event) {
+    }
+}
+
+// takeaways:
+/*
+ * -world.isRemote (true = client, false = server)
+ * -
+ */
